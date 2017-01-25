4e95049aafb661e3f1f8f7cd78d1e9401a25daf2
rakeshangre <rakesh.angre@creowntech.com>
2017-01-25 17:05:24 +0530

    Added data project

---
+++ CreownTutorWeb/CreownTutor.Data/Repository/AccountRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreownTutor.Data.Repository
+{
+    public class AccountRepository
+    {
+        public void Register()
+        {
+
+        }
+
+        public void Login()
+        {
+
+        }
+    }
+}
