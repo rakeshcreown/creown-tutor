e681df07b172fad721c87770a870fb7115317b81
rakeshangre <rakesh.angre@creowntech.com>
2017-01-30 17:37:54 +0530

    Course Search and listing page implementation

--- CreownTutorWeb/CreownTutor.Data/Repository/CourseRepository.cs
+++ CreownTutorWeb/CreownTutor.Data/Repository/CourseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CreownTutor.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,38 @@ namespace CreownTutor.Data.Repository
 {
     public class CourseRepository : BaseRepository
     {
-        public List<Category> ShowCategories(string searchKeyword = "")
+        public List<Model.Category> GetCategories()
         {
-            if (!string.IsNullOrEmpty(searchKeyword))
+            List<Model.Category> categories = new List<Model.Category>();
+            foreach (var item in dbEntity.Categories.ToList())
             {
-                return dbEntity.Categories.Where(c => c.Category1.ToLower().Contains(searchKeyword.ToLower())).ToList();
+                int count = 0;
+                count = dbEntity.Courses.Where(c => c.Category == item.CategoryID.ToString()).Count();
+                categories.Add(new Model.Category() { CategoryID = item.CategoryID.ToString(), CategoryName = item.Category1, CourseCount = count });
             }
-            return dbEntity.Categories.ToList();
+            return categories;
         }
 
-        public void ShowCourses(string category = "")
+        public List<Course> GetCourses(string category = "", string search = "")
         {
+            if (!string.IsNullOrEmpty(category))
+            {
+                return dbEntity.Courses.Where(c => c.Category == category).ToList();
+            }
+            if (!string.IsNullOrEmpty(search))
+            {
+                return dbEntity.Courses.Where(c => c.CourseName.Contains(search) || c.CourseDescription.Contains(search)).ToList();
+            }
+            return dbEntity.Courses.ToList();
+        }
 
+        public List<Course> GetLatestCourse(int ct = 0)
+        {
+            if (ct == 0)
+            {
+                return dbEntity.Courses.OrderByDescending(c => c.CreatedDateAndTime).ToList();
+            }
+            return dbEntity.Courses.OrderByDescending(c => c.CreatedDateAndTime).ToList().Take(ct).ToList();
         }
     }
 }
--- CreownTutorWeb/CreownTutorWeb/Controllers/CourseController.cs
+++ CreownTutorWeb/CreownTutorWeb/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
-﻿using CreownTutor.Data.Repository;
+﻿using CreownTutor.Data.Model;
+using CreownTutor.Data.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,25 @@ namespace CreownTutorWeb.Controllers
     public class CourseController : Controller
     {
         // GET: Category
-        public ActionResult Index()
+        public ActionResult Index(string cid = "", string s = "")
         {
+            CourseViewModel model = new CourseViewModel();
             CourseRepository catRepo = new CourseRepository();
-            var categories = catRepo.ShowCategories();
-            return View();
+            model.Categories = catRepo.GetCategories();
+            model.Courses = catRepo.GetCourses(cid, s);
+            model.LatestCourses = catRepo.GetLatestCourse(3);
+            model.SearchKeyword = s;
+            if (!string.IsNullOrEmpty(cid))
+            {
+                model.Category = cid;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Search(CourseViewModel model)
+        {
+            return RedirectToAction("Index", "Course", new { cid = model.Category, s = model.SearchKeyword });
         }
     }
 }
