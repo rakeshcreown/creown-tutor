37128e1590d8de14ae68862603498fcaad0d611b
ApurvaCreown <apurva.uchil@creowntech.com>
2017-02-22 11:56:45 +0530

    Revert checkin in Student Controller

--- CreownTutorWeb/CreownTutorWeb/Controllers/StudentController.cs
+++ CreownTutorWeb/CreownTutorWeb/Controllers/StudentController.cs
@@ -11,14 +11,14 @@ namespace CreownTutorWeb.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        public ActionResult Index(bool isEnrolled,int id=6)
+        public ActionResult Index(int id=6)
         {
             Session["user"] = "Student";
             Student student = new Student();
             StudentRepository studentrepo = new StudentRepository();
             student.Courses = studentrepo.GetLatestCourseByTeacher(id);
             student.User = studentrepo.GetStudents(id);
-            student.CourseRegistrations = studentrepo.GetRegisteredCourses(isEnrolled);
+            //student.CourseRegistrations = studentrepo.GetRegisteredCourses(isEnrolled);
             return View(student);
         }
     }
